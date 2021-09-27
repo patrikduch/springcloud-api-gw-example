edd7a1f683b722f6ccb9eb8972bf918b75fbe4a0
patrikduch <duchpatrik@icloud.com>
2021-09-27 02:35:52 +0200

    Initial commit

---
+++ src/main/java/com/example/springcloudgw/SpringCloudGwApplication.java
@@ -0,0 +1,25 @@
+package com.example.springcloudgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringCloudGwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGwApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+
+}
---
+++ src/test/java/com/example/springcloudgw/SpringCloudGwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
